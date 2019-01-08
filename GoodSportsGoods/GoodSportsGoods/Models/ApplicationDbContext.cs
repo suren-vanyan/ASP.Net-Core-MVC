dbb0a3e6d0848b2fff29386a806f387fe704af8b
suren-vanyan <vanyansuren@gmail.com>
2019-01-08 18:17:57 +0400

    Creating the Database Classes

---
+++ GoodSportsGoods/GoodSportsGoods/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GoodSportsGoods.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products;
+    }
+}
