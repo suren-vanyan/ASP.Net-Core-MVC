ff562bbde8ca16e899da4e87797284312c7ac2a8
suren-vanyan <vanyansuren@gmail.com>
2019-02-17 16:02:45 +0400

    Call forwarding

--- Metanit/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ Metanit/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -11,64 +11,21 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        //public IActionResult AddUser(User user)
-        //{
-        //    //string userInfo = $"Id: {user.Id}  Name: {user.Name}  Age: {user.Age}  HasRight: {user.HasRight}";
-        //    //return Content(userInfo);
-        //    if (ModelState.IsValid)
-        //    {
-        //        string userInfo = $"Id: {user.Id}  Name: {user.Name}  Age: {user.Age}  HasRight: {user.HasRight}";
-        //        return Content(userInfo);
-        //    }
-        //    return Content($"Количество ошибок: {ModelState.ErrorCount}");
-
-        //}
-
-        public IActionResult AddUser1()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddUser1([FromQuery] User user)
-        {
-            string userInfo = $"Name: {user.Name}  Age: {user.Age}";
-            return Content(userInfo);
-        }
-
-        public IActionResult AddUser2([FromBody] User user)
-        {
-            string userInfo = $"Name: {user.Name}  Age: {user.Age}";
-            return Content(userInfo);
-        }
-
         public IActionResult GetUserAgent([FromHeader(Name = "User-Agent")] string userAgent)
         {
             return Content(userAgent);
         }
 
-
-        public IActionResult GetData(string[] items)
+        public IActionResult AddUser()
         {
-            string result = "";
-            foreach (var item in items)
-                result += item + "; ";
-            return Content(result);
+            return View();
         }
 
-        public IActionResult GetPhone1(Phone myPhone)
+        [HttpPost]
+        public IActionResult AddUser([FromQuery] User user)
         {
-            if (ModelState.IsValid)
-                return Content($"Name: {myPhone?.Name}  Price:{myPhone.Price}  Company: {myPhone?.Manufacturer?.Name}");
-            return Content($"Count of Eror  {ModelState.ErrorCount}");
+            string userInfo = $"Name: {user.Name}  Age: {user.Age}";
+            return Content(userInfo);
         }
-
-        public IActionResult GetPhone([Bind("Name,Price,Manufacturer")]Phone myPhone)
-        {
-            if (ModelState.IsValid)
-                return Content($"Name: {myPhone?.Name}  Price:{myPhone.Price}  Company: {myPhone?.Manufacturer?.Name}");
-            return Content($"Count of Eror  {ModelState.ErrorCount}");
-        }
-
     }
 }
