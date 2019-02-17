0511109bf2320346bbb09314d16dbd8dbb451495
suren-vanyan <vanyansuren@gmail.com>
2019-02-17 22:43:43 +0400

    Filters

---
+++ Metanit/WebAppFilters/WebAppFilters/Filters/SimpleActionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppFilters.Filters
+{
+    public class SimpleActionFilter:Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // реализация отсутствует
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.Now.ToString("dd/MM/yyyy hh-mm-ss"));
+        }
+    }
+}
