932a07f7a776233ebdfeaed0ebe30437070a8308
suren-vanyan <vanyansuren@gmail.com>
2019-03-02 23:48:12 +0400

    Bower Conf

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+         => View(new Dictionary<string, string>
+         {
+             ["Message "] = "This is t he Index action "
+         });
+    }
+}
