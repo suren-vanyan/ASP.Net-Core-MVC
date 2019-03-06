d834dfb249ad1de0be0d482bca5610e4c540e6b3
suren-vanyan <surenvanyan@gmail.com>
2019-03-06 16:39:40 +0400

    Web API

---
+++ ControllersApi/ControllersApi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ControllersApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository Repository { get; set; }
+
+        public HomeController(IRepository repo)
+        {
+            Repository = repo;
+        }
+
+        public ViewResult Index() => View(Repository.Reservations);
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            Repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ControllersApi/ControllersApi/Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersApi.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string ClientName { get; set; }
+        public string Location { get; set; }
+    }
+}
