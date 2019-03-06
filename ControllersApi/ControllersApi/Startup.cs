bf26e3cd679db8169b87e6de92cdbdc8394d58a0
suren-vanyan <vanyansuren@gmail.com>
2019-03-06 16:42:56 +0400

    Web Api

--- ControllersApi/ControllersApi/Startup.cs
+++ ControllersApi/ControllersApi/Startup.cs
@@ -20,6 +20,7 @@ namespace ControllersApi
             services.AddMvc();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
