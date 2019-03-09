f360eb8a626f803a4c01b9c70a7b084f38fb1e0c
suren-vanyan <vanyansuren@gmail.com>
2019-03-09 23:07:07 +0400

    Add Poco

---
+++ ControllersAndActions/ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class PocoController
+    {
+        public string Index() =>
+            "This is a Poco Controller";
+    }
+}
