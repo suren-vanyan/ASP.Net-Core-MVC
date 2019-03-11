6b2d4f480c41205a0190369ff5078c35a1cbda3b
suren-vanyan <vanyansuren@gmail.com>
2019-03-11 23:53:33 +0400

    Add Domain Models

---
+++ OnionApp/OnionApp.Domain.Interfaces/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnionApp.Domain.Core;
+
+namespace OnionApp.Domain.Interfaces
+{
+    interface IBookRepository
+    {
+        IEnumerable<Book> GetBookList();
+        Book GetBook(int id);
+        void Create(Book item);
+        void Update(Book item);
+        void Delete(int id);
+        void Save();
+
+    }
+}
