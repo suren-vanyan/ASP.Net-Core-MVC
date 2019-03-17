25765e53caf0f4e50c174bf7a4bfa57ae5ca9225
suren-vanyan <vanyansuren@gmail.com>
2019-03-17 16:51:31 +0400

    depend

---
+++ DependencyInjection/DependencyInjection/Models/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        Product this[string name] { get; }
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+    }
+}
---
+++ DependencyInjection/DependencyInjection/Models/MemoryRepositroy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    class MemoryRepositroy : IRepository
+    {
+        private Dictionary<string, Product> products;
+
+        public MemoryRepositroy()
+        {
+            products = new Dictionary<string, Product>();
+            new List<Product>
+            {
+                new Product{Name="Kajak",Price=15000},
+                new Product{Name="LifeJaket",Price=20000},
+                new Product{Name="Soccer Ball",Price=8000},
+            }.ForEach(p => AddProduct(p));
+        }
+
+        public Product this[string name] => products[name];
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product product)
+        {
+            products[product.Name]=product;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            products.Remove(product.Name);
+        }
+    }
+}
