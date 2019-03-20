18b11c57465dab458b1f4c0dfcc001ab30fccbff
suren-vanyan <vanyansuren@gmail.com>
2019-03-20 12:24:38 +0400

    email sending

--- Metanit/EmailService/EmailService/EmailServices.cs
+++ Metanit/EmailService/EmailService/EmailServices.cs
@@ -11,6 +11,7 @@ namespace EmailService
     {
         public async Task SendEmailAsync(string email, string subject, string message)
         {
+
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress("Junior C# .Net Developer", "vanyansuren@gmail.com"));
             emailMessage.To.Add(new MailboxAddress("Suren", email));
@@ -22,7 +23,7 @@ namespace EmailService
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.ConnectAsync("smtp.gmail.com", 25, false);
                 await client.AuthenticateAsync("vanyansuren@gmail.com", "password");
                 await client.SendAsync(emailMessage);
 
