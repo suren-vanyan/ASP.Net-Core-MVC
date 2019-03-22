060a20b1fc964130563eb289f587291462a99400
suren-vanyan <vanyansuren@gmail.com>
2019-03-22 11:49:29 +0400

    configuration

---
+++ Metanit/Chapter4-Configuration/Chapter4-Configuration/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chapter4_Configuration
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddIniFile("config.json")
+                .AddEnvironmentVariables()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    {"firstname", "Tom"},
+                    {"age", "31"}
+                });
+            AppConfiguration = builder.Build();
+        }
+        public IConfiguration AppConfiguration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var color = AppConfiguration["color"];
+            var text = AppConfiguration["text"];
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"<p style='color:{color};'>{text}</p>");
+            });
+        }
+    }
+}
