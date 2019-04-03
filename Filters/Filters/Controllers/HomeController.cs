21ab98f9bc4bccf230554c0116f038838ab9a37e
suren-vanyan <vanyansuren@gmail.com>
2019-04-03 22:41:19 +0400

    Filters

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index => View("Message ","This is the Index action on the Home controller");
+    }
+}
