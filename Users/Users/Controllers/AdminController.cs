504d0222cb3373269be8396c402ae3e179ac881b
suren-vanyan <vanyansuren@gmail.com>
2019-04-11 23:56:46 +0400

    add validation

--- Users/Users/Controllers/AdminController.cs
+++ Users/Users/Controllers/AdminController.cs
@@ -8,12 +8,19 @@ using Users.Models;
 
 namespace Users.Controllers
 {
-    public class AdminController:Controller
+    public class AdminController : Controller
     {
         private UserManager<AppUser> UserManager;
-        public AdminController(UserManager<AppUser> userManager)
+        private IUserValidator<AppUser> userValidator;
+        private IPasswordValidator<AppUser> passwordValidator;
+        private IPasswordHasher<AppUser> passwordHasher;
+        public AdminController(UserManager<AppUser> userManager, IUserValidator<AppUser> userValidator,
+            IPasswordValidator<AppUser> passwordValidator, IPasswordHasher<AppUser> passwordHasher)
         {
             UserManager = userManager;
+            this.userValidator = userValidator;
+            this.passwordValidator = passwordValidator;
+            this.passwordHasher = passwordHasher;
         }
 
         public ViewResult Index() => View(UserManager.Users);
@@ -41,7 +48,7 @@ namespace Users.Controllers
                         ModelState.AddModelError("", error.Description);
                     }
                 }
-                
+
             }
             return View(model);
         }
@@ -74,6 +81,66 @@ namespace Users.Controllers
             {
                 ModelState.AddModelError("", error.Description);
             }
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+                return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email,string password)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail= await userValidator.ValidateAsync(UserManager, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(UserManager,
+                    user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user,
+                        password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if ((validEmail.Succeeded && validPass == null)
+                || (validEmail.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await UserManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
         }
     }
 }
--- Users/Users/Startup.cs
+++ Users/Users/Startup.cs
@@ -54,6 +54,7 @@ namespace Users
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
+            //899Rus,
         }
     }
 }
