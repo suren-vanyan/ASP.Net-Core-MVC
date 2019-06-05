748caba3c0b8bd505660220b37becf7ebd65f782
suren-vanyan <vanyansuren@gmail.com>
2019-06-06 00:57:53 +0400

    add identity

--- WorkingWithVisualStudio/WorkingWithVisualStudio/wwwroot/js/third.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    var element = document.createElement("p");
-    element.textContent = "This is the element from the (modified) third.js file";
-    document.querySelector("body").appendChild(element);
-});
