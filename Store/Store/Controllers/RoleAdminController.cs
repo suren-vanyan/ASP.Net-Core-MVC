7c128160d9d0da9dc853921d164908e31e54627f
suren-vanyan <vanyansuren@gmail.com>
2019-06-10 23:52:57 +0400

    get username by role

---
+++ Store/Store/Controllers/RoleAdminController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index() => View(_roleManager.Roles);
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result =await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                    AddErrorsFromResult(result);
+            }
+
+            return View(name);
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+           IdentityRole role=await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                    AddErrorsFromResult(result);
+            }
+            else { ModelState.AddModelError("", "Role not found"); }
+
+            return View(nameof(Index), _roleManager.Roles);
+        }
+
+        private void AddErrorsFromResult(IdentityResult identityResult)
+        {
+            foreach (var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+        }
+    }
+}
---
+++ Store/Store/Infrastructure/RoleUsersTagHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Infrastructure
+{
+    [HtmlTargetElement(ParentTag = "td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper : TagHelper
+    {
+        private UserManager<User> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public RoleUsersTagHelper(UserManager<User> userManager,
+                                  RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HtmlAttributeName("identity-role")]
+        public string RoleId { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            var role = await _roleManager.FindByIdAsync(RoleId);
+            if (role != null)
+            {
+                foreach (var user in _userManager.Users)
+                {
+                    if (user != null && await _userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+
+            output.Content.SetContent(names.Count == 0 ? "No User": string.Join(", ", names));
+        }
+    }
+}
