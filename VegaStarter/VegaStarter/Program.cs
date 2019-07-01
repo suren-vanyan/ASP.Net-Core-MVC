d3ae8020db7aebd836129d1045a498c39a7a5321
suren-vanyan <vanyansuren@gmail.com>
2019-07-02 01:26:51 +0400

    Cretae Vega Project Template

---
+++ VegaStarter/VegaStarter/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace VegaStarter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                        var env = hostingContext.HostingEnvironment;
+                        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                              .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                                  optional: true, reloadOnChange: true);
+                        config.AddEnvironmentVariables();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                   logging.AddConsole();
+                   logging.AddDebug();
+                   logging.AddEventSourceLogger();
+               })
+                .UseStartup<Startup>();
+    }
+}
